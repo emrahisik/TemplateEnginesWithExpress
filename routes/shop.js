b587c09e1b2e0e9682dccb8f7bb7a3227d5e4773
emrahisik <emrahisk@gmail.com>
2023-04-19 23:56:35 -0400

    shop html template delivered

--- routes/shop.js
+++ routes/shop.js
@@ -4,7 +4,7 @@ const products = require('./admin').products;
 
 router.get('/',(req,res,next)=>{
   console.log('sh', products)
-  res.status(200).sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
+  res.render('shop', { docTitle: 'My Shop', products: products });
 });
 
 module.exports = router;
