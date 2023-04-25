40d4af2d44b0abbfb86706d0b66882a09d6b039f
emrahisik <emrahisk@gmail.com>
2023-04-25 12:32:02 -0400

    clean-up

--- routes/shop.js
+++ routes/shop.js
@@ -3,7 +3,6 @@ const path = require("path");
 const products = require('./admin').products;
 
 router.get('/',(req,res,next)=>{
-  console.log('sh', products)
   res.status(200).sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
 });
 
