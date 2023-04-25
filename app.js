14df7f0203581c022de151749c6ff2846533be79
emrahisik <emrahisk@gmail.com>
2023-04-25 10:43:37 -0400

    EJS Implemented

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require("path");
-const { engine } = require('express-handlebars')
+// const { engine } = require('express-handlebars')
 
 const app = express();
 
 // Register the template engine then set it in express
-app.engine('hbs', engine({layoutsDir: 'views/layout', defaultLayout: 'main-layout', extname: 'hbs'}))
-app.set('view engine', 'hbs');
+// app.engine('hbs', engine({layoutsDir: 'views/layout', defaultLayout: 'main-layout', extname: 'hbs'}))
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 // var bodyParser = require('body-parser');
 const adminRouter = require('./routes/admin');
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname,'public')))
 app.use('/admin',adminRouter.router);
 app.use(shopRouter);
 app.use((req,res,next)=>{
-  res.status(404).render('404',{docTitle: '404 Not Found', layout: 'main-layout'})
+  res.status(404).render('404',{docTitle: '404 Not Found'})
 })
 
 app.listen(3000, ()=> console.log('Listening at Port 3000'))
