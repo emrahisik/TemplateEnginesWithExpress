783bdd2e60ae854fdeef16856649abe20e88edf4
emrahisik <emrahisk@gmail.com>
2023-06-01 17:31:22 -0400

    admin routes registered

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
-const { getAddProduct, postAddProduct } = require('../controllers/products.controller')
+const { getAddProduct, postAddProduct, getProducts } = require('../controllers/admin.controller')
 
 router
   .route("/add-product")
   .get(getAddProduct)
   .post(postAddProduct);
+
+router.route('/products').get(getProducts)
 
 module.exports = router;
