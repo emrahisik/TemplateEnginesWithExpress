9553cb35a0e152b5d8d935208e18258fb6c15901
emrahisik <emrahisk@gmail.com>
2023-06-01 17:31:35 -0400

    shop routes registered

--- routes/shop.js
+++ routes/shop.js
@@ -1,6 +1,11 @@
 const router = require("express").Router();
-const { getProducts } = require("../controllers/products.controller");
+const { getCart, getCheckout, getProducts, getOrders, getProduct, postCart } = require("../controllers/shop.controller");
 
 router.route('/').get(getProducts);
+router.route('/cart').get(getCart).post(postCart);
+router.route('/products').get(getProducts)
+router.route('/products/:productId').get(getProduct)
+router.route('/checkout').get(getCheckout)
+router.route('/orders').get(getOrders)
 
 module.exports = router;
